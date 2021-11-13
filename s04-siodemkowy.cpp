b6c6fed67ae30f9e14da885cd5c363f27e5123fd
s24580 <92123980+s24580@users.noreply.github.com>
2021-11-13 21:21:16 +0100

    Add files via upload

---
+++ s04-siodemkowy.cpp
@@ -0,0 +1,42 @@
+/*2. Napisz program, który pobierze jako argument liczbę zapisaną w systemie dziesiętnym i przeliczy ją na system siódemkowy.
+Wynik wydrukuj na std::cout. Zapisz program jako s04-siodemkowy.cpp*/
+
+#include <iostream>
+#include <vector>
+
+auto main ()-> int {
+ int x,y,z,i;
+ std::vector <int> tab ;
+
+	std::cout << "podaj lizczbe: ";
+	std::cin >> x;
+	
+	 
+	while (x > 0) 
+	{
+		y = x%7 ;
+		tab.push_back(y);
+		x /= 7 ;
+		
+	} 
+	z = tab.size();	
+	if (tab[z] == 0){
+		for (int j = tab.size()-1 ; j >= 0  ; j--){
+			std::cout << tab [ j ];
+		}
+	
+	}
+	else {
+		for (int j = tab.size() ; j >= 0  ; j--){
+			std::cout << tab [ j ];
+		}
+	}
+
+	std::cout << "\n";
+
+return 0; 	
+
+
+
+
+}
