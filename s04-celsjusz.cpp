de977c1b5d391555ac2e635dfb6474536c4370c3
s24580 <92123980+s24580@users.noreply.github.com>
2021-11-14 21:31:50 +0100

    Add files via upload

---
+++ s04-celsjusz.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+class Celsius {
+public:	
+	int temp;
+	
+	std::string C ;
+	auto to_string() const -> std::string;
+};
+
+
+auto main () -> int
+{
+	Celsius temperatur;
+	std::cin >> temperatur;
+	Celsius_temperatur.string C = "°C";
+	std::cout << "Put temp";
+	std::cin >> temperatur.C;
+if (temp <= -273.15)
+	{ 
+		std::cout << "-273.15" << Celsius_temperatur.C << "\n";
+	}
+	else
+	{
+		std::cout << Celsius.temperatur << Celsius_temperatur.C <<"\n";
+	}
+	
+return 0;
+
+}
+// nie wiem pomocy :')
